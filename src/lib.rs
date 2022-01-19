3199bd7711604b578ec95f10fe92e05ee8aac8fe
esf <efrain.sotelo.ferry@gmail.com>
2022-01-19 23:59:07 +0100

    feat: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,62 @@ use na::DMatrix;
 #[derive(Debug)]
 pub struct Mode {
     pub frequency: Vec<f64>,
-    pub eigenvalues: DMatrix<f64>,
-    pub eigenvectors_normalized: DMatrix<f64>,
+    pub eigenvalues: DMatrix<f64>, // Also known as Spectral Matrix
+    pub eigenvectors_normalized: DMatrix<f64>, //Also known as Modal Matrix
     pub eigenvectors: DMatrix<f64>,
 }
+//In general, it is assumed that the diagonal entries of the spectral
+//matrix and the columns in the modal matrix are ordered according to the
+//increasing magnitudes of the eigenvalues BUT in this case is not returned in that way
 
-pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Result<Mode, String> {
+// Properties:
+
+// Orthonality property
+// For an eigenvalue (lambda_i) and eigenvector (x_i), the eigenvalue problem equation [K]*x = lambda*[M]*x can be
+// rewriten as [K]*x_a = lambda_a*[M]*x_a , another pair can be [K]*x_b = lambda_b*[M]*x_b
+// The property makes:
+//  x_b_trans * [M] * x_a = 0
+//  x_b_trans * [K] * x_a = 0
+pub fn check_orthogonality_property(
+    mass: DMatrix<f64>,
+    stiff: DMatrix<f64>,
+    eigenvectors: DMatrix<f64>,
+) -> bool {
+    let dim = eigenvectors.ncols();
+    let vec_dim_num: Vec<usize> = (0..dim).collect();
+
+    println!("Inputs: {}{}{}", mass, stiff, eigenvectors);
+    for (i, col) in eigenvectors.column_iter().enumerate() {
+        // col.mul_to(&mass, out)
+        // let other_cols: Vec<usize> = vec_dim_num.iter().clone().filter(|&x| i.eq(x)).collect();
+        let mut other_cols = vec_dim_num.clone();
+        other_cols.retain(|&x| !x.eq(&i));
+        for other_col_idx in other_cols {
+            println!("{:?} {:?}", i, other_col_idx);
+            println!(
+                "{}{}{}",
+                col.transpose(),
+                mass,
+                eigenvectors.column(other_col_idx)
+            );
+            println!(
+                "Check {}",
+                col.transpose()
+                    .mul(&mass)
+                    .mul(eigenvectors.column(other_col_idx))
+            )
+        }
+        // let other_cols = vec_dim_num.clone().retain(|&x| x != i);
+        // println!("HEHETEETETEE{} {}", col.transpose().mul(&mass), i);
+        // println!("ASEHNNTE{}, {:?}, {:?}", i, vec_dim_num, other_cols);
+    }
+    true
+}
+// Expansion theorem or Principle of modal superposition
+
+// Lib functions
+
+pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: &DMatrix<f64>) -> Result<Mode, String> {
     //Checks
     if !mass_matrix.is_square() {
         return Err(String::from("Mass matrix is not square"));
@@ -30,7 +80,7 @@ pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Resu
 
     let dim = shape_mass.0;
 
-    println!("M_matrix : {}", mass_matrix);
+    // println!("M_matrix : {}", mass_matrix);
     // let inv_m_matrix = m_matrix.clone().try_inverse();
     let inv_m_matrix = match mass_matrix.clone().try_inverse() {
         Some(i) => i,
@@ -43,7 +93,7 @@ pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Resu
         Some(i) => i,
         None => return Err(String::from("Error getting eigenvalues")),
     };
-    println!("A_matrix :{}", a_matrix);
+    // println!("A_matrix :{}", a_matrix);
 
     let mut eigen_vect: DMatrix<f64> = DMatrix::from_vec(dim, dim, vec![0.0; dim * dim]);
 
@@ -55,6 +105,8 @@ pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Resu
         // Hence, we can use either equation to solve for the ratios of the unknowns, therefore we
         // set b_vec[i]=1.0
         b_vec[i] = 1.0;
+        // Other way can be to set always the first value to 1.0
+        // b_vec[0] = 1.0;
         let b = DMatrix::from_vec(dim, 1, b_vec);
         let decomp = a_mat_for_eigen_vec.lu();
         // let mut x = decomp.solve(&b).expect("Linear resolution failed.");
@@ -63,13 +115,15 @@ pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Resu
             None => return Err(String::from("Linear resolution failed")),
         };
         x.div_assign(x[i]);
+        // This way corresponds to setting first value to 1.0 in b_vec
+        // x.div_assign(x[0]);
         // add_sub_matrix(&mut eigen_vect, (0, i), &x).expect("Error adding sub_matrix");
         match add_sub_matrix(&mut eigen_vect, (0, i), &x) {
             Ok(_) => (),
             Err(e) => return Err(e),
         };
     }
-    println!("---------------------------------------------------------");
+    // println!("---------------------------------------------------------");
 
     let omega = eigen.clone().map(|e| e.sqrt()).as_slice().to_vec();
     // let om2 = eigen.clone().map(|e| e.sqrt());
@@ -77,7 +131,7 @@ pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Resu
     // println!("EigenValues : {}", eigen);
     // println!("EigenVectors : {}", eigen_vect);
 
-    println!("---------------------------------------------------------");
+    // println!("---------------------------------------------------------");
 
     // Get modal mass and modal stiffness matrix
     let eigen_vect_trans = eigen_vect.transpose();
@@ -99,12 +153,12 @@ pub fn eigen(mass_matrix: &DMatrix<f64>, stiffness_matrix: DMatrix<f64>) -> Resu
     }
 
     // println!("eigenvectors norm : {}", eigen_vect_norm);
-    let modal_mass_norm = eigen_vect_norm
-        .transpose()
-        .clone()
-        .mul(mass_matrix.clone())
-        .mul(eigen_vect_norm.clone());
-    println!("modal mass norm : {}", modal_mass_norm);
+    // let modal_mass_norm = eigen_vect_norm
+    //     .transpose()
+    //     .clone()
+    //     .mul(mass_matrix.clone())
+    //     .mul(eigen_vect_norm.clone());
+    // println!("modal mass norm : {}", modal_mass_norm);
     //
     // let diag_c = eigen.clone().div_assign(2.0);
     // println!("Diag_c : {}", diag_c);
@@ -175,7 +229,7 @@ fn add_sub_matrix(
 #[cfg(test)]
 mod tests {
 
-    use na::DVector;
+    use na::{DVector, Matrix2x4, Vector2};
 
     use super::*;
     #[test]
@@ -187,7 +241,7 @@ mod tests {
         let m_matrix: DMatrix<f64> = DMatrix::from_vec(dim, dim, mass_mat_vec);
         let k_matrix: DMatrix<f64> = DMatrix::from_vec(dim, dim, stiff_mat_vec);
 
-        let modes = eigen(&m_matrix, k_matrix);
+        let modes = eigen(&m_matrix, &k_matrix);
         assert!(modes.is_ok());
         let modes = modes.unwrap();
         //Print
@@ -210,5 +264,39 @@ mod tests {
         assert!(modes
             .eigenvectors_normalized
             .relative_eq(&sol_eigenvec_norm, eps, eps));
+
+        println!(
+            "Xt * M * X : {}",
+            modes
+                .eigenvectors_normalized
+                .transpose()
+                .mul(&m_matrix)
+                .mul(&modes.eigenvectors_normalized)
+        );
+        let generalized_mass = modes
+            .eigenvectors
+            .transpose()
+            .mul(&m_matrix)
+            .mul(&modes.eigenvectors);
+        println!("Xt * M * X : {}", generalized_mass);
+        // check_orthogonality_property(m_matrix, k_matrix, modes.eigenvectors_normalized);
     }
+    // #[test]
+    // fn vec_mul_mat() {
+    //     let vec: DVector<f64> = DVector::from_vec(vec![1.0, 1.0]);
+    //     let matrix: DMatrix<f64> = DMatrix::from_vec(2, 2, vec![1.0, 0.0, 0.0, 2.0]);
+    //     //     println!("{}", vec.transpose());
+    //     //     println!("{}", matrix);
+    //     //     println!("{}", vec.transpose().mul(matrix));
+    // }
+    // #[test]
+    // fn vec_mul_mat_compile() {
+    //     let vec: Vector2<f64> = Vector2::from_vec(vec![1.0, 1.0]);
+    //     let matrix: Matrix2x4<f64> =
+    //         Matrix2x4::from_vec(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+    //     // println!("{}", vec.mul(matrix))
+    //     // println!("{}", vec.transpose());
+    //     // println!("{}", matrix);
+    //     // println!("{}", vec.transpose().mul(matrix));
+    // }
 }
