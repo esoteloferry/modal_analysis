64110e50dbee3d96e767bc305b063a77e68bc768
ESF <esf@goldwinddenmark.com>
2022-10-31 16:10:37 +0100

    feat:  add rigid mode test

--- src/modal/modes_test.rs
+++ src/modal/modes_test.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 pub mod test {
     extern crate nalgebra as na;
+    use std::ops::MulAssign;
+
     use super::super::modes::*;
     use na::{DMatrix, DVector};
 
@@ -10,30 +12,33 @@ pub mod test {
         pub mass_matrix: DMatrix<f64>,
         pub stiff_matrix: DMatrix<f64>,
         pub frequencies: DVector<f64>,
-        pub eigenvalues: DMatrix<f64>,
+        pub eigenvalues: DVector<f64>,
         pub eigenvectors_normalized: DMatrix<f64>,
     }
 
     pub struct Setup {
         pub _2d: Modal,
-        // pub _2d_with_rigid_mode: Modal,
+        pub _2d_with_rigid_mode: Modal,
     }
 
     impl Setup {
         pub fn new() -> Self {
             let dim = 2;
+
+            // ***** Rigid modes *****
+            // Example 4.6.1 - Engineering vibration 2nd edition - Daniel J Inman
             let mass_mat_vec: Vec<f64> = vec![9.0, 0.0, 0.0, 1.0]; //kg
             let stiff_mat_vec: Vec<f64> = vec![27.0, -3.0, -3.0, 3.0]; //N/m
 
             let m_matrix: DMatrix<f64> = DMatrix::from_vec(dim, dim, mass_mat_vec);
             let k_matrix: DMatrix<f64> = DMatrix::from_vec(dim, dim, stiff_mat_vec);
             //Solution
-            let (w1_2d, w2_2d) = (1.53819, 0.79623); //rad/s
-            let sol_freq_vec_2d = DVector::from_vec(vec![w1_2d, w2_2d]);
-            let sol_eigenval_2d =
-                DMatrix::from_vec(dim, dim, vec![w1_2d * w1_2d, 0.0, 0.0, w2_2d * w2_2d]);
-            let sol_eigenvec_norm_2d =
-                DMatrix::from_vec(dim, dim, vec![0.88807, -0.32506, 0.4597, 0.62796]); // X1 and X2 [m] (real)
+            let lambda1_2d: f64 = 2.0;
+            let lambda2_2d: f64 = 4.0;
+            let sol_freq_vec_2d = DVector::from_vec(vec![lambda1_2d.sqrt(), lambda2_2d.sqrt()]);
+            let sol_eigenval_2d = DVector::from_vec(vec![lambda1_2d, lambda2_2d]);
+            let mut sol_eigenvec_norm_2d = DMatrix::from_vec(dim, dim, vec![1.0, 1.0, 1.0, -1.0]); // X1 and X2 [m] (real)
+            sol_eigenvec_norm_2d.mul_assign(0.7071);
             let _2d: Modal = Modal {
                 mass_matrix: m_matrix,
                 stiff_matrix: k_matrix,
@@ -41,9 +46,53 @@ pub mod test {
                 eigenvalues: sol_eigenval_2d,
                 eigenvectors_normalized: sol_eigenvec_norm_2d,
             };
+            // ***** Rigid modes *****
+            // Example 4.4.4 - Engineering vibration 2nd edition - Daniel J Inman
+            let mass_mat_vec: Vec<f64> = vec![1.0, 0.0, 0.0, 4.0]; //kg
+            let m_matrix: DMatrix<f64> = DMatrix::from_vec(dim, dim, mass_mat_vec);
+            let mut k_matrix: DMatrix<f64> =
+                DMatrix::from_vec(dim, dim, vec![1.0, -1.0, -1.0, 1.0]);
+            k_matrix.mul_assign(400.0);
+            // Solution
+            let lambda1_2d: f64 = 0.0;
+            let lambda2_2d: f64 = 5.0;
+            let sol_freq_vec_2d = DVector::from_vec(vec![lambda1_2d.sqrt(), lambda2_2d.sqrt()]);
+            let sol_eigenval_2d = DVector::from_vec(vec![lambda1_2d, lambda2_2d]);
+            let mut sol_eigenvec_norm_2d = DMatrix::from_vec(dim, dim, vec![1.0, 2.0, -2.0, 1.0]);
+            sol_eigenvec_norm_2d.mul_assign(0.4472);
+            let _2d_with_rigid_mode: Modal = Modal {
+                mass_matrix: m_matrix,
+                stiff_matrix: k_matrix,
+                frequencies: sol_freq_vec_2d,
+                eigenvalues: sol_eigenval_2d,
+                eigenvectors_normalized: sol_eigenvec_norm_2d,
+            };
 
-            Self { _2d }
+            //
+            Self {
+                _2d,
+                _2d_with_rigid_mode,
+            }
         }
+    }
+    #[test]
+    fn eigen_2d() {
+        let setup = Setup::new();
+        let sol = eigen(&setup._2d.mass_matrix, &setup._2d.stiff_matrix);
+        assert!(sol.is_ok());
+        let sol_data = sol.unwrap();
+
+        assert!(sol_data
+            .frequencies
+            .relative_eq(&setup._2d.frequencies, EPS, EPS));
+        assert!(sol_data
+            .eigenvalues
+            .relative_eq(&setup._2d.eigenvalues, EPS, EPS));
+        assert!(sol_data.eigenvectors_normalized.relative_eq(
+            &setup._2d.eigenvectors_normalized,
+            EPS,
+            EPS
+        ));
     }
 
     #[test]
@@ -54,4 +103,10 @@ pub mod test {
         let sol_data = sol.unwrap();
         sol_data.report();
     }
+    // #[test]
+    // fn matrix_order() {
+    //     let mut mat = DMatrix::from_vec(2, 2, vec![1.0, 2.0, 3.0, 4.0]);
+    //     mat.mul_assign(2.0);
+    //     println!("Hello {}", mat);
+    // }
 }
